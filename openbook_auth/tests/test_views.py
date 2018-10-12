2473abb53e1e54aa29b6fc7e75be69e9a2d5e658
Joel Hernandez <joel@open-book.org>
2018-10-12 11:12:50 +0200

    :white_check_mark: test username & email check APIs

--- openbook_auth/tests/test_views.py
+++ openbook_auth/tests/test_views.py
@@ -167,3 +167,127 @@ class RegistrationAPITests(APITestCase):
 
     def _get_url(self):
         return reverse('register-user')
+
+
+class UsernameCheckAPITests(APITestCase):
+    """
+    UsernameCheckAPI
+    """
+
+    def test_username_not_taken(self):
+        """
+        should return status 202 if username is not taken.
+        """
+        username = 'lifenautjoe'
+        request_data = {'username': username}
+        url = self._get_url()
+        response = self.client.post(url, request_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_username_taken(self):
+        """
+        should return status 400 if the username is taken
+        """
+        username = 'lifenautjoe'
+        User.objects.create_user(username=username, password='SuChSeCuRiTyWow!', email='lifenautjoe@mail.com')
+        request_data = {'username': username}
+        url = self._get_url()
+        response = self.client.post(url, request_data, format='json')
+
+        parsed_response = json.loads(response.content)
+
+        self.assertIn('username', parsed_response)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_username(self):
+        """
+        should return 400 if the username is not a valid one
+        """
+        url = self._get_url()
+        usernames = ('lifenau!', 'p-o-t-a-t-o', '.a!', 'dexter@', '🤷‍♂️')
+        for username in usernames:
+            data = {
+                'username': username
+            }
+            response = self.client.post(url, data, format='json')
+            parsed_response = json.loads(response.content)
+            self.assertIn('username', parsed_response)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_valid_username(self):
+        """
+        should return 202 if the username is a valid username
+        """
+        url = self._get_url()
+        usernames = ('lifenautjoe', 'shantanu_123', 'm4k3l0v3n0tw4r', 'o_0')
+        for username in usernames:
+            data = {
+                'username': username
+            }
+            response = self.client.post(url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def _get_url(self):
+        return reverse('username-check')
+
+
+class EmailCheckAPITests(APITestCase):
+    """
+    EmailCheckAPI
+    """
+
+    def test_email_not_taken(self):
+        """
+        should return status 202 if email is not taken.
+        """
+        email = 'joel@open-book.org'
+        request_data = {'email': email}
+        url = self._get_url()
+        response = self.client.post(url, request_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_email_taken(self):
+        """
+        should return status 400 if the email is taken
+        """
+        email = 'joel@open-book.org'
+        User.objects.create_user(email=email, password='SuChSeCuRiTyWow!', username='lifenautjoe')
+        request_data = {'email': email}
+        url = self._get_url()
+        response = self.client.post(url, request_data, format='json')
+
+        parsed_response = json.loads(response.content)
+
+        self.assertIn('email', parsed_response)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_email(self):
+        """
+        should return 400 if the email is not a valid one
+        """
+        url = self._get_url()
+        emails = ('not-a-valid-email.com', 'fake-email.com', 'joel hernandez', 'omelette@dufromage', 'test_data!asd')
+        for email in emails:
+            data = {
+                'email': email
+            }
+            response = self.client.post(url, data, format='json')
+            parsed_response = json.loads(response.content)
+            self.assertIn('email', parsed_response)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_valid_email(self):
+        """
+        should return 202 if the email is a valid email
+        """
+        url = self._get_url()
+        emails = ('joel@open-book.org', 'gerald@rivia.com', 'obi@wan.com', 'c3po@robot.me')
+        for email in emails:
+            data = {
+                'email': email
+            }
+            response = self.client.post(url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def _get_url(self):
+        return reverse('email-check')
