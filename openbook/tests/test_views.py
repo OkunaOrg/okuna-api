06eba90c2ab225cc6f0e9b5519e712eb6bc30287
Joel Hernandez <joel@open-book.org>
2018-10-15 10:54:03 +0200

    :white_check_mark: test timezone set with Time-Zone header

---
+++ openbook/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class TimeAPITests(APITestCase):
+    """
+    TimeAPITests
+    """
+
+    def test_timezone_set(self):
+        """
+        should set the timezone provided in the Time-Zone header
+        """
+        url = self._get_url()
+        timezone_to_set = 'America/Mexico_City'
+        header = {'HTTP_TIME_ZONE': timezone_to_set}
+        response = self.client.get(url, **header)
+        parsed_response = json.loads(response.content)
+        self.assertEqual(parsed_response['timezone'], timezone_to_set)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def _get_url(self):
+        return reverse('time')
--- openbook/views.py
+++ openbook/views.py
@@ -1,9 +1,7 @@
 from django.utils.timezone import get_current_timezone
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from openbook.responses import ApiMessageResponse
-from django.utils.translation import gettext as _
 from django.utils import timezone
 
 
@@ -14,7 +12,10 @@ class Time(APIView):
 
     def get(self, request):
         # The serializer contains the username checks, meaning at this line, it's all good.
-        return ApiMessageResponse(
-            _('The time is %(time)s on timezone %(timezone)s') % {'time': timezone.localtime(timezone.now()),
-                                                                  'timezone': get_current_timezone()},
-            status=status.HTTP_202_ACCEPTED)
+        time = timezone.localtime(timezone.now())
+        time_zone = get_current_timezone()
+
+        return Response({
+            'time': time,
+            'timezone': str(time_zone)
+        }, status=status.HTTP_200_OK)
