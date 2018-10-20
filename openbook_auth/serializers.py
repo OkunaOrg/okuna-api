f539ace228b6ecd48f496000e4fde18e5c6e0770
evict <github@evicted.ninja>
2018-10-20 17:57:27 +0200

    :art: Consistent username length values
    Changed inconsistent username values to 30.

--- openbook_auth/serializers.py
+++ openbook_auth/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class RegisterSerializer(serializers.Serializer):
-    password = serializers.CharField(min_length=8, max_length=100, validators=[validate_password])
+    password = serializers.CharField(min_length=10, max_length=100, validators=[validate_password])
     birth_date = serializers.DateField(input_formats=["%d-%m-%Y"])
     username = serializers.CharField(min_length=1, max_length=30,
                                      validators=[username_characters_validator, username_not_taken_validator])
@@ -28,7 +28,7 @@ class EmailCheckSerializer(serializers.Serializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(min_length=1, max_length=30,
                                      validators=[username_characters_validator])
-    password = serializers.CharField(min_length=8, max_length=100)
+    password = serializers.CharField(min_length=10, max_length=100)
 
 
 class GetUserProfileSerializer(serializers.ModelSerializer):
