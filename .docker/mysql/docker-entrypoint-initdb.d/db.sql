3c16d000edc718ecf746ffabd572c7ef089bd418
Fabio D'Amico <fdamico@worth.systems>
2018-11-02 17:52:29 +0100

    :wrench: Changing configuration files because passing .env to a mysql container is enough, we don't need db.sql + created a dedicated volume for mysql

--- .docker/mysql/docker-entrypoint-initdb.d/db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS openbook_db_api;
-CREATE USER 'openbookuser'@'localhost' IDENTIFIED BY 'changeme';
-GRANT ALL PRIVILEGES ON openbook_db_api.* TO 'openbookuser'@'localhost';
