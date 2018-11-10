88b241fcb53dd31dd68e847a1b6288a02b93cb62
Joel Hernandez <joel@open-book.org>
2018-11-10 13:50:57 +0100

    :rewind: make create post PUT again

--- openbook_posts/views.py
+++ openbook_posts/views.py
@@ -14,7 +14,7 @@ class Posts(APIView):
     permission_classes = (IsAuthenticated,)
     parser_classes = (MultiPartParser, FormParser, JSONParser)
 
-    def post(self, request):
+    def put(self, request):
 
         request_data = request.data.dict()
 
