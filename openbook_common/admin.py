7a679e61b84f5f759fdbf0e4fac0dfe38237a238
Joel Hernandez <joel@open-book.org>
2018-11-19 14:51:36 +0100

    :sparkles: add emoji admin model

--- openbook_common/admin.py
+++ openbook_common/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from openbook_common.models import Emoji
+
+
+class EmojiAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'created',
+        'shortcut',
+        'color',
+    )
+
+
+admin.site.register(Emoji, EmojiAdmin)
