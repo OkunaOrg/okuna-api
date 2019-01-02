c32bebc29a43784e3673a7695db604be227fae7d
Shantanu <shantanu.tarey@kpn.com>
2019-01-02 14:26:33 +0100

    ✨ management command for kickstarter

---
+++ openbook_invitations/management/commands/import_invites.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction, IntegrityError
+from openbook_invitations.models import UserInvite
+from openbook_invitations.parsers import kickstarter_csv_parser
+
+
+class Command(BaseCommand):
+    help = 'Imports backer data into UserInvite models'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--kickstarter', type=str, help='Import from kickstarter csv')
+        parser.add_argument('--indiegogo', type=str, help='Import from indiegog typeform')
+
+    def handle(self, *args, **options):
+
+        if options['kickstarter']:
+            filepath = options['kickstarter']
+            try:
+                with transaction.atomic():
+                    kickstarter_csv_parser(filepath)
+            except IntegrityError as e:
+                print('IntegrityError %s ' % e)
+            self.stdout.write(self.style.SUCCESS('Successfully imported data'))
