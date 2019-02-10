bb5dab128b87352517791b4302e5c0b195948882
Joel Hernandez <joel@open-book.org>
2019-02-10 16:02:42 +0100

    :white_check_mark: test get community members

--- openbook_communities/tests/views/community/members/test_views.py
+++ openbook_communities/tests/views/community/members/test_views.py
@@ -7,7 +7,7 @@ import logging
 import json
 
 from openbook_common.tests.helpers import make_user, make_authentication_headers_for_user, \
-    make_community, make_fake_post_text
+    make_community
 
 logger = logging.getLogger(__name__)
 fake = Faker()
@@ -18,16 +18,91 @@ class CommunityMembersAPITest(APITestCase):
         """
         should be able to retrieve the members of a public community
         """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        other_user = make_user()
+        community = make_community(creator=other_user, type='P')
+        community_name = community.name
+
+        amount_of_community_members = 5
+        community_members_ids = [
+            other_user.pk
+        ]
+
+        for i in range(0, amount_of_community_members):
+            community_member = make_user()
+            community_member.join_community_with_name(community_name=community_name)
+            community_members_ids.append(community_member.pk)
+
+        url = self._get_url(community_name=community.name)
+        response = self.client.get(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_members = json.loads(response.content)
+
+        self.assertEqual(len(response_members), len(community_members_ids))
+
+        for response_member in response_members:
+            response_member_id = response_member.get('id')
+            self.assertIn(response_member_id, community_members_ids)
 
     def test_cannot_retrieve_members_of_private_community(self):
         """
         should not be able to retrieve the members of a private community
         """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        other_user = make_user()
+        community = make_community(creator=other_user, type='T')
+
+        url = self._get_url(community_name=community.name)
+        response = self.client.get(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_can_retrieve_members_of_private_community_part_of(self):
         """
         should be able to retrieve the members of a private community part of
         """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        other_user = make_user()
+        community = make_community(creator=other_user, type='T')
+        community_name = community.name
+
+        other_user.invite_user_with_username_to_community_with_name(username=user.username,
+                                                                    community_name=community_name)
+        user.join_community_with_name(community_name=community_name)
+
+        amount_of_community_members = 5
+        community_members_ids = [
+            other_user.pk,
+            user.pk
+        ]
+
+        for i in range(0, amount_of_community_members):
+            community_member = make_user()
+            other_user.invite_user_with_username_to_community_with_name(username=community_member.username,
+                                                                        community_name=community_name)
+            community_member.join_community_with_name(community_name=community_name)
+            community_members_ids.append(community_member.pk)
+
+        url = self._get_url(community_name=community.name)
+        response = self.client.get(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_members = json.loads(response.content)
+
+        self.assertEqual(len(response_members), len(community_members_ids))
+
+        for response_member in response_members:
+            response_member_id = response_member.get('id')
+            self.assertIn(response_member_id, community_members_ids)
 
     def _get_url(self, community_name):
         return reverse('community-members', kwargs={
