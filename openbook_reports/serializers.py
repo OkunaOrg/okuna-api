b609eea9a8c96e592fbea2efe972e850958517e5
Shantanu <shantanu.tarey@kpn.com>
2019-03-12 19:54:14 +0100

    🐛 add community etc to serializer as we need it while showing reports

--- openbook_reports/serializers.py
+++ openbook_reports/serializers.py
@@ -4,10 +4,13 @@ from rest_framework.serializers import ModelSerializer
 
 from openbook_auth.models import User, UserProfile
 from openbook_auth.serializers import BadgeSerializer
-from openbook_common.serializers_fields.post import PostReportsField, PostReportCountsField
+from openbook_circles.models import Circle
+from openbook_common.models import Emoji
+from openbook_common.serializers_fields.post import PostReportsField, PostReportCountsField, ReactionField, \
+    CirclesField, CommentsCountField, IsMutedField, ReactionsEmojiCountField
 from openbook_communities.models import Community
 from openbook_communities.validators import community_name_characters_validator, community_name_exists
-from openbook_posts.models import Post, PostVideo, PostImage, PostComment
+from openbook_posts.models import Post, PostVideo, PostImage, PostComment, PostReaction
 from openbook_posts.validators import post_id_exists, post_comment_id_exists, post_uuid_exists
 from openbook_reports.models import ReportCategory, PostReport, PostCommentReport
 from openbook_reports.validators import is_valid_report_category, report_id_exists, comment_report_id_exsits
@@ -177,6 +180,16 @@ class PostCommunitySerializer(serializers.ModelSerializer):
         )
 
 
+class PostCircleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Circle
+        fields = (
+            'id',
+            'name',
+            'color',
+        )
+
+
 class PostReportCommentSerializer(serializers.ModelSerializer):
     category = PostReportCategorySerializer()
 
@@ -191,6 +204,34 @@ class PostReportCommentSerializer(serializers.ModelSerializer):
         )
 
 
+class PostReactionEmojiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Emoji
+
+        fields = (
+            'id',
+            'keyword',
+            'image',
+            'order',
+        )
+
+
+class PostReactionSerializer(serializers.ModelSerializer):
+    emoji = PostReactionEmojiSerializer(many=False)
+
+    class Meta:
+        model = PostReaction
+        fields = (
+            'emoji',
+            'id'
+        )
+
+
+class PostEmojiCountSerializer(serializers.Serializer):
+    emoji = PostReactionEmojiSerializer(many=False)
+    count = serializers.IntegerField(required=True, )
+
+
 class PostReportCategoryCountSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=settings.REPORT_CATEGORY_TITLE_MAX_LENGTH)
     count = serializers.IntegerField()
@@ -200,18 +241,33 @@ class AuthenticatedUserPostSerializer(serializers.ModelSerializer):
     image = PostImageSerializer(many=False)
     video = PostVideoSerializer(many=False)
     creator = PostCreatorSerializer(many=False)
+    reactions_emoji_counts = ReactionsEmojiCountField(emoji_count_serializer=PostEmojiCountSerializer)
+    reaction = ReactionField(reaction_serializer=PostReactionSerializer)
+    comments_count = CommentsCountField()
+    circles = CirclesField(circle_serializer=PostCircleSerializer)
+    community = PostCommunitySerializer()
+    is_muted = IsMutedField()
     reports = PostReportsField(post_report_serializer=PostReportSerializer)
 
     class Meta:
         model = Post
         fields = (
+            'id',
             'uuid',
+            'comments_count',
+            'reactions_emoji_counts',
             'created',
             'text',
             'image',
             'video',
             'creator',
-            'reports'
+            'reaction',
+            'public_comments',
+            'public_reactions',
+            'circles',
+            'community',
+            'reports',
+            'is_muted'
         )
 
 
