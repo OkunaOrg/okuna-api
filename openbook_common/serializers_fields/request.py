dc3a25f9d43083e63bdc13b200bb47adcdbc63bb
Joel Hernandez <joel@open-book.org>
2019-03-13 17:20:32 +0100

    :bug: fix cannot update to fully qualified bio url

--- openbook_common/serializers_fields/request.py
+++ openbook_common/serializers_fields/request.py
@@ -4,6 +4,6 @@ from rest_framework.fields import URLField
 class FriendlyUrlField(URLField):
     def to_internal_value(self, data):
         if isinstance(data, str):
-            if 'https://' not in data or 'http://' not in data:
+            if 'https://' not in data and 'http://' not in data:
                 data = 'https://' + data
         return data
