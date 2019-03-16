d47f6b0089897553ee580b1769be4b1a1bbf6200
Joel Hernandez <joel@open-book.org>
2019-03-16 23:17:07 +0100

    :fire: remove notification post creator because thats us..

--- openbook_notifications/serializers.py
+++ openbook_notifications/serializers.py
@@ -62,31 +62,9 @@ class PostCommentPostImageSerializer(serializers.ModelSerializer):
         )
 
 
-class NotificationPostCreatorProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = (
-            'id',
-            'avatar'
-        )
-
-
-class NotificationPostCreatorSerializer(serializers.ModelSerializer):
-    profile = NotificationPostCreatorProfileSerializer()
-
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'username',
-            'profile'
-        )
-
-
 class NotificationPostSerializer(serializers.ModelSerializer):
     image = PostCommentPostImageSerializer()
     video = PostCommentPostVideoSerializer()
-    creator = NotificationPostCreatorSerializer()
 
     class Meta:
         model = Post
@@ -96,7 +74,6 @@ class NotificationPostSerializer(serializers.ModelSerializer):
             'image',
             'text',
             'video',
-            'creator',
         )
 
 
