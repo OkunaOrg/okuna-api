3c3972ee2295fa99baba3e43a9f5589f0e41f875
Joel Hernandez <joel@open-book.org>
2019-04-14 17:29:58 +0200

    :sparkles: add min_id to get_timeline_posts

--- openbook_auth/models.py
+++ openbook_auth/models.py
@@ -1352,13 +1352,13 @@ class User(AbstractUser):
 
         return profile_posts
 
-    def get_timeline_posts(self, lists_ids=None, circles_ids=None, max_id=None):
+    def get_timeline_posts(self, lists_ids=None, circles_ids=None, max_id=None, min_id=None):
         """
         Get the timeline posts for self. The results will be dynamic based on follows and connections.
         """
 
         if not circles_ids and not lists_ids:
-            return self._get_timeline_posts_with_no_filters(max_id=max_id)
+            return self._get_timeline_posts_with_no_filters(max_id=max_id, min_id=min_id)
 
         return self._get_timeline_posts_with_filters(max_id=max_id, circles_ids=circles_ids, lists_ids=lists_ids)
 
@@ -1402,7 +1402,7 @@ class User(AbstractUser):
         Post = get_post_model()
         return Post.objects.filter(timeline_posts_query).distinct()
 
-    def _get_timeline_posts_with_no_filters(self, max_id=None):
+    def _get_timeline_posts_with_no_filters(self, max_id=None, min_id=None):
         """
         Being the main action of the network, an optimised call of the get timeline posts call with no filtering.
         """
@@ -1410,6 +1410,12 @@ class User(AbstractUser):
 
         # Add all own posts
         timeline_posts_query = Q(creator=self.pk)
+
+        if max_id:
+            timeline_posts_query.add(Q(id__lt=max_id), Q.AND)
+
+        if min_id:
+            timeline_posts_query.add(Q(id_gt=min_id), Q.AND)
 
         # Add all community posts
         timeline_posts_query.add(Q(community__memberships__user__id=self.pk), Q.OR)
@@ -1422,9 +1428,6 @@ class User(AbstractUser):
         followed_users_ids = [followed_user['followed_user_id'] for followed_user in followed_users]
 
         timeline_posts_query.add(Q(creator__in=followed_users_ids, circles__id=world_circle_id), Q.OR)
-
-        if max_id:
-            timeline_posts_query.add(Q(id__lt=max_id), Q.AND)
 
         Post = get_post_model()
 
