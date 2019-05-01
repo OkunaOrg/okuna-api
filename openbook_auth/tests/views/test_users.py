2d70201f61378d4ffdf3742fde4202bbcb73bd31
Joel Hernandez <joel@open-book.org>
2019-05-01 13:23:21 +0200

    :white_check_mark: test block/unblock APIs

--- openbook_auth/tests/views/test_users.py
+++ openbook_auth/tests/views/test_users.py
@@ -179,3 +179,183 @@ class GetUserAPITests(APITestCase):
         return reverse('get-user', kwargs={
             'user_username': user.username
         })
+
+
+class BlockUserAPITests(APITestCase):
+    """
+    BlockUserAPI
+    """
+    fixtures = [
+        'openbook_circles/fixtures/circles.json'
+    ]
+
+    def test_can_block_user(self):
+        """
+        should be able to block a user and return 200
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        user_to_block = make_user()
+
+        url = self._get_url(user_to_block)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(user.has_blocked_user_with_id(user_id=user_to_block.pk))
+
+    def test_cannot_block_already_blocked_user(self):
+        """
+        should not be able to block an already blocked user and return 403
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        user_to_block = make_user()
+        user.block_user_with_id(user_id=user_to_block.pk)
+
+        url = self._get_url(user_to_block)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(user.has_blocked_user_with_id(user_id=user_to_block.pk))
+
+    def test_cannot_block_blocking_user(self):
+        """
+        should not be able to block a blocking user and return 400
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        blocking_user = make_user()
+        blocking_user.block_user_with_id(user_id=user.pk)
+
+        url = self._get_url(blocking_user)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(blocking_user.has_blocked_user_with_id(user_id=user.pk))
+
+    def test_disconnects_from_user(self):
+        """
+        should disconnect from the blocked user
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        user_to_block = make_user()
+
+        user.connect_with_user_with_id(user_id=user_to_block.pk)
+        user_to_block.confirm_connection_with_user_with_id(user_id=user.pk)
+
+        url = self._get_url(user_to_block)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(user.is_connected_with_user_with_id(user_id=user_to_block.pk))
+
+    def test_unfollows_user(self):
+        """
+        should unfollow the blocked user
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        user_to_block = make_user()
+
+        user.follow_user_with_id(user_id=user_to_block.pk)
+
+        url = self._get_url(user_to_block)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(user.is_following_user_with_id(user_id=user_to_block.pk))
+
+    def test_gets_unfollowed_by_blocked_user(self):
+        """
+        should be unfollowed by the blocked user
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        user_to_block = make_user()
+
+        user_to_block.follow_user_with_id(user_id=user.pk)
+
+        url = self._get_url(user_to_block)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(user_to_block.is_following_user_with_id(user_id=user_to_block.pk))
+
+    def _get_url(self, user):
+        return reverse('block-user', kwargs={
+            'user_username': user.username
+        })
+
+
+class UnblockUserAPITests(APITestCase):
+    """
+    UnblockUserAPI
+    """
+
+    def test_can_unblock_blocked_user(self):
+        """
+        should be able to unblock a blocked user and return 200
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        blocked_user = make_user()
+        user.block_user_with_id(user_id=blocked_user.pk)
+
+        url = self._get_url(blocked_user)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(user.has_blocked_user_with_id(user_id=blocked_user.pk))
+
+    def test_cannot_unblock_not_blocked_user(self):
+        """
+        should not be able to unblock a not blocked user and return 400
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        blocked_user = make_user()
+
+        url = self._get_url(blocked_user)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(user.has_blocked_user_with_id(user_id=blocked_user.pk))
+
+    def test_cannot_unblock_unblocking_user(self):
+        """
+        should not be able to unblock a blocking user and return 400
+        """
+        user = make_user()
+        headers = make_authentication_headers_for_user(user)
+
+        blocking_user = make_user()
+        blocking_user.block_user_with_id(user_id=user.pk)
+
+        url = self._get_url(blocking_user)
+
+        response = self.client.post(url, **headers)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(blocking_user.has_blocked_user_with_id(user_id=user.pk))
+
+    def _get_url(self, user):
+        return reverse('unblock-user', kwargs={
+            'user_username': user.username
+        })
