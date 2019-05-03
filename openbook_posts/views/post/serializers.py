2eaac107ab35b99f05173e4e8e0108b402407b01
Shantanu <shantanutarey@gmail.com>
2019-05-03 19:27:47 +0200

    :sparkles: add views for reply endpoints

--- openbook_posts/views/post/serializers.py
+++ openbook_posts/views/post/serializers.py
@@ -484,3 +484,60 @@ class OpenClosePostSerializer(serializers.ModelSerializer):
             'uuid',
             'is_closed',
         )
+
+
+class CommentRepliesPostSerializer(serializers.Serializer):
+    post_uuid = serializers.UUIDField(
+        validators=[post_uuid_exists],
+        required=True,
+    )
+    post_comment_id = serializers.IntegerField(
+        validators=[post_comment_id_exists],
+        required=True,
+    )
+    text = serializers.CharField(max_length=settings.POST_COMMENT_MAX_LENGTH, required=True, allow_blank=False)
+
+
+class PostCommentReplySerializer(serializers.ModelSerializer):
+    commenter = PostCommenterField(post_commenter_serializer=PostCommentCommenterSerializer,
+                                   community_membership_serializer=PostCommenterCommunityMembershipSerializer)
+
+    class Meta:
+        model = PostComment
+        fields = (
+            'commenter',
+            'text',
+            'created',
+            'parent_comment',
+            'is_edited',
+            'id'
+        )
+
+
+class GetPostCommentRepliesSerializer(serializers.Serializer):
+    post_uuid = serializers.UUIDField(
+        validators=[post_uuid_exists],
+        required=True,
+    )
+    post_comment_id = serializers.IntegerField(
+        validators=[post_comment_id_exists],
+        required=True,
+    )
+    max_id = serializers.IntegerField(
+        required=False,
+    )
+    min_id = serializers.IntegerField(
+        required=False,
+    )
+    count_max = serializers.IntegerField(
+        required=False,
+        max_value=20
+    )
+    count_min = serializers.IntegerField(
+        required=False,
+        max_value=20
+    )
+    sort = serializers.ChoiceField(required=False, choices=[
+        'ASC',
+        'DESC'
+    ])
