c5c3af4e067892bacbe20cc33f257486f27cd405
andy obrien <andy@1bg.com>
2019-05-18 17:17:17 -0600

    Update test classnames

--- openbook_auth/tests/test_models.py
+++ openbook_auth/tests/test_models.py
@@ -7,12 +7,12 @@ from openbook_auth.models import User
 logger = logging.getLogger(__name__)
 
 
-class UserModelTests(APITestCase):
+class UserEmailFieldTests(APITestCase):
     def test_format_email_to_common_string(self):
         """
         should format f.o.o@gmail.com to foo@gmail.com
         """
-        model = User()
+        model = User.EmailField()
 
         actual = 'f.o.o@gmail.com'
         expected = 'foo@gmail.com'
