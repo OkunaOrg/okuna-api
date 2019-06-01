f9819c325643c04b32b67d15e46e55bd32fde75a
Andy OBrien <javabudd@gmail.com>
2019-06-01 13:19:18 -0600

    Exec passthrough command

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+# start the redis server
 redis-server &
 
 cd /opt/openbook-api
 
+# install pip env deps, run migrations, collect media, start the server
 pipenv install
 pipenv run python manage.py migrate
 echo "yes" | pipenv run python manage.py collectmedia
 pipenv run python manage.py loaddata circles.json emoji-groups.json emojis.json badges.json categories.json
 
-pipenv run python manage.py runserver 0.0.0.0:80
+exec $@
