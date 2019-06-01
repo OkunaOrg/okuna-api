7fcd82a4c1c85df6e737c1adf11b669b49989c4b
Andy OBrien <javabudd@gmail.com>
2019-06-01 13:18:21 -0600

    Run pipenv install and migrations before starting webserver

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,4 +3,11 @@ set -eo pipefail
 
 redis-server &
 
-exec $@
+cd /opt/openbook-api
+
+pipenv install
+pipenv run python manage.py migrate
+echo "yes" | pipenv run python manage.py collectmedia
+pipenv run python manage.py loaddata circles.json emoji-groups.json emojis.json badges.json categories.json
+
+pipenv run python manage.py runserver 0.0.0.0:80
