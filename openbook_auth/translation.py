63126aaa8512c49a9dffaabfb18db402a62fbd06
Shantanu <shantanutarey@gmail.com>
2019-06-20 16:35:57 +0200

    :arrow_up: add langdetect package

--- openbook_auth/translation.py
+++
@@ -1,9 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from openbook_auth.models import UserLanguage
-
-
-class UserLanguageTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-
-translator.register(UserLanguage, UserLanguageTranslationOptions)
