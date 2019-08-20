d9718da4c8f1c4de504828f577835a45faec5569
Joel Hernandez <joel@open-book.org>
2019-08-20 20:03:17 +0200

    :bug: fix wrong reverse url lookup

--- openbook_posts/tests/views/test_post_image.py
+++ openbook_posts/tests/views/test_post_image.py
@@ -110,6 +110,6 @@ class PostImageAPITests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def _get_url(self, post):
-        return reverse('post-images', kwargs={
+        return reverse('post-image', kwargs={
             'post_uuid': post.uuid
         })
