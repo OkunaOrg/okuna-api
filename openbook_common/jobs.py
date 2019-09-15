881f4dcbfc5064e7a6c977b15eaec2fdf1e8d8d7
evict <github@evicted.ninja>
2019-09-15 13:03:15 +0200

    :pencil2: TRESHOLD -> THRESHOLD, woops

--- openbook_common/jobs.py
+++ openbook_common/jobs.py
@@ -3,8 +3,8 @@ from django_rq.decorators import job
 from openbook_common.utils.rq_helpers import RQStats
 from openbook_common.helpers import send_alert_to_channel
 
-from openbook.settings import FAILED_JOB_TRESHOLD
-from openbook.settings import ACTIVE_JOB_TRESHOLD, ACTIVE_WORKER_TRESHOLD
+from openbook.settings import FAILED_JOB_THRESHOLD
+from openbook.settings import ACTIVE_JOB_THRESHOLD, ACTIVE_WORKER_THRESHOLD
 
 
 @job
@@ -13,7 +13,7 @@ def verify_failed_job_threshold():
     rq_stats = RQStats()
     failed_job_count = rq_stats.get_failed_job_count()
 
-    if failed_job_count >= FAILED_JOB_TRESHOLD:
+    if failed_job_count >= FAILED_JOB_THRESHOLD:
         send_alert_to_channel(f"*OH NOES: we haz too many failed jobs "
                               f"({failed_job_count})!!*")
 
@@ -24,7 +24,7 @@ def verify_active_job_threshold():
     rq_stats = RQStats()
     active_job_count = rq_stats.get_active_job_count()
 
-    if active_job_count >= ACTIVE_JOB_TRESHOLD:
+    if active_job_count >= ACTIVE_JOB_THRESHOLD:
         send_alert_to_channel(f"*UH-OH: we have way too many active jobs right "
                               f"now: {active_job_count}!!*")
 
@@ -35,6 +35,6 @@ def verify_active_worker_threshold():
     rq_stats = RQStats()
     active_worker_count = rq_stats.get_active_worker_count()
 
-    if active_worker_count >= ACTIVE_WORKER_TRESHOLD:
+    if active_worker_count >= ACTIVE_WORKER_THRESHOLD:
         send_alert_to_channel(f"*Hmm, we are not supposed to have "
                               f"{active_worker_count} workers*")
