d74f6935f787b97f4103ee7603fd8761085d02ec
Shantanu <shantanutarey@gmail.com>
2019-09-27 15:06:15 +0200

    :sparkles: add limit option

--- openbook_invitations/management/commands/allocate_invites.py
+++ openbook_invitations/management/commands/allocate_invites.py
@@ -10,23 +10,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--count', type=int, help='Count by which each users invites will be increased')
+        parser.add_argument('--limit', type=int, help='Limit invites to max amount for each user, use with --count')
         parser.add_argument('--total', type=int, help='Total final amount to which each users invites will be set')
         parser.add_argument('--username', type=str, help='Username to allocate invites to')
 
     def handle(self, *args, **options):
         username = None
+        limit = None
 
         if options['username']:
             username = str(options['username'])
 
+        if options['limit']:
+            limit = int(options['limit'])
+
         if options['count']:
             count = int(options['count'])
-            self.handle_increase_count(count, username)
+            self.handle_increase_count(count, username, limit)
         elif options['total']:
             count = int(options['total'])
             self.handle_set_total_count(count, username)
 
-    def handle_increase_count(self, count, username):
+    def handle_increase_count(self, count, username, limit):
         User = get_user_model()
         if username is not None:
             users = User.objects.filter(username=username)
@@ -38,8 +43,19 @@ class Command(BaseCommand):
 
         for user in users:
             try:
-                user.invite_count = F('invite_count') + count
-                user.save()
+                if limit:
+                    # ensure new count is less than equal to limit
+                    current_count = user.invite_count
+
+                    if current_count < limit:
+                        new_count = current_count + count
+                        if new_count > limit:
+                            new_count = limit
+                        user.invite_count = new_count
+                        user.save()
+                else:
+                    user.invite_count = F('invite_count') + count
+                    user.save()
             except IntegrityError as e:
                 print('IntegrityError %s '.format(e))
                 self.stderr.write('Error during allocation for user %s'.format(user.username))
