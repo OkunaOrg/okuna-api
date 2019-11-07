2f60fc5431c7e2d9bc98fd08a989414353525f1a
Joel Hernandez <joel@open-book.org>
2019-11-07 15:01:40 +0100

    :wrench: make okuna up non-daemon

--- okuna-cli.py
+++ okuna-cli.py
@@ -5,12 +5,16 @@ import logging
 import os.path
 from shutil import copyfile
 import json
+import atexit
+import os
+import requests
+from halo import Halo
 
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
     '%(log_color)s%(name)s -> %(message)s'))
 
-logger = colorlog.getLogger('🤖 Okuna CLI')
+logger = colorlog.getLogger('🤖')
 logger.addHandler(handler)
 
 logger.setLevel(level=logging.DEBUG)
@@ -20,9 +24,34 @@ current_dir = os.path.dirname(__file__)
 OKUNA_CLI_CONFIG_FILE = os.path.join(current_dir, '.okuna-cli.json')
 OKUNA_CLI_CONFIG_FILE_TEMPLATE = os.path.join(current_dir, 'templates/.okuna-cli.json')
 
+OKUNA_API_ADDRESS = '127.0.0.1'
+OKUNA_API_PORT = 80
+
 CONTEXT_SETTINGS = dict(
     default_map={}
 )
+
+
+def _check_okuna_api_is_running():
+    # Create a TCP socket
+    try:
+        response = requests.get('http://%s:%s/health/' % (OKUNA_API_ADDRESS, OKUNA_API_PORT))
+        response_status = response.status_code
+        return response_status == 200
+    except requests.ConnectionError as e:
+        return False
+
+
+def _print_okuna_logo():
+    print(r"""
+   ____  _                      
+  / __ \| |                     
+ | |  | | | ___   _ _ __   __ _ 
+ | |  | | |/ | | | | '_ \ / _` |
+ | |__| |   <| |_| | | | | (_| |
+  \____/|_|\_\\__,_|_| |_|\__,_|
+                                
+  """)
 
 
 def _file_exists(filename):
@@ -59,26 +88,40 @@ def cli():
     pass
 
 
+def _down():
+    """Bring Okuna down"""
+    logger.error('⬇️  Bringing Okuna down...')
+    subprocess.run(["docker-compose", "down"])
+
+
 @click.command()
 def up():
     """Bring Okuna up"""
-    _ensure_was_bootstrapped()
-    logger.info('Bringing services up...')
+    _print_okuna_logo()
+    logger.info('⬆️  Bringing Okuna up...')
+
+    atexit.register(_down)
     subprocess.run(["docker-compose", "up", "-d"])
-    logger.info('Okuna docker service started successfully')
+    spinner = Halo(text='Waiting for server to come up...', spinner='dots')
+    spinner.start()
 
+    is_running = _check_okuna_api_is_running()
 
-@click.command()
-def down():
-    """Bring Okuna down"""
-    logger.info('Bringing services down...')
-    subprocess.run(["docker-compose", "down"])
+    while not is_running:
+        is_running = _check_okuna_api_is_running()
+
+    spinner.stop()
+
+    logger.info('✅️  Okuna is live at http://%s:%s' % (OKUNA_API_ADDRESS, OKUNA_API_PORT))
+    input("⌨️  Press enter to exit")
+    # logger.info('Okuna docker service started successfully')
+    # subprocess.run(["docker", 'exec', '-it', 'okuna-api', '/bin/bash', '-c', 'echo hello'])
 
 
 @click.command()
 def status():
     """Bring Okuna down"""
-    logger.info('Checking services status...')
+    logger.info('🕵️‍♂️  Retrieving services status...')
     subprocess.run(["docker-compose", "ps"])
 
 
@@ -89,7 +132,6 @@ def bootstrap():
 
 
 cli.add_command(up)
-cli.add_command(down)
 cli.add_command(bootstrap)
 cli.add_command(status)
 
