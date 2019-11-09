16fe926b3c0808e500c5aaf4d4aa96b761216b5c
evict <github@evicted.ninja>
2019-11-09 15:45:36 +0100

    :fire: Removed tests and updated README

--- openbook_common/management/commands/worker_health_check.py
+++ openbook_common/management/commands/worker_health_check.py
@@ -1,3 +1,4 @@
+from sys import exit
 from logging import getLogger
 
 from django.core.management.base import BaseCommand
@@ -25,6 +26,9 @@ class Command(BaseCommand):
             if failed_job_count >= FAILED_JOB_THRESHOLD:
                 send_alert_to_channel(f"*OH NOES: we haz too many failed jobs "
                                       f"in {queue}: ({failed_job_count})!!*")
+                print(f"{queue} has too many failed jobs {failed_job_count}")
+
+                self.retval += 1
 
             active_job_count = rq_stats.get_active_job_count()
 
@@ -33,13 +37,24 @@ class Command(BaseCommand):
                         f"*UH-OH: we have way too many active jobs "
                         f"in {queue} right now: {active_job_count}!!*"
                                       )
+                print(f"{queue} has too many jobs {active_job_count}")
+                self.retval += 1
+
             active_worker_count = rq_stats.get_active_worker_count()
 
             if active_worker_count >= ACTIVE_WORKER_THRESHOLD:
                 send_alert_to_channel(f"*Hmm, we are not supposed to have "
                                       f"{active_worker_count} workers in "
                                       f"{queue}*")
+                print(f"{queue} has too many workers {active_worker_count}")
+
+                self.retval += 1
 
     def handle(self, *args, **options):
 
+        self.retval = 1
         self.verify_worker_health()
+
+        # the return code will be equal to the amount of threshold
+        # violations
+        exit(self.retval)
