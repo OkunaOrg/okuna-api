2d1fa455ad98f30fa0e6079e5b646ef308fc8622
Joel Hernandez <joel@open-book.org>
2019-12-02 15:46:08 +0100

    :recycle: make random hashtag colors darker

--- openbook_common/utils/helpers.py
+++ openbook_common/utils/helpers.py
@@ -50,11 +50,22 @@ def generate_random_hex_color():
     return '#%02X%02X%02X' % (r(), r(), r())
 
 
-def get_random_pastel_color():
+def get_random_pastel_color(brightness_offset=None):
     # Its a random color jeez, we dont need cryptographically secure randomness
     h, s, l = random.random(), 0.5 + random.random() / 2.0, 0.4 + random.random() / 5.0  # nosec
     r, g, b = [int(256 * i) for i in colorsys.hls_to_rgb(h, l, s)]
     return '#%02x%02x%02x' % (r, g, b)
+
+
+def get_color_brightness_variant(hex_color, brightness_offset=1):
+    """ takes a color like #87c95f and produces a lighter or darker variant """
+    if len(hex_color) != 7:
+        raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
+    rgb_hex = [hex_color[x:x + 2] for x in [1, 3, 5]]
+    new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+    new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int]  # make sure new values are between 0 and 255
+    # hex() produces "0x88", we want just "88"
+    return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
 
 
 def delete_file_field(filefield):
