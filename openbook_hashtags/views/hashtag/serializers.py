72664d3a58563452d20993ab456153a97cae211d
Joel Hernandez <joel@open-book.org>
2019-12-03 13:14:15 +0100

    :bug: fix missing is_encircled in field

--- openbook_hashtags/views/hashtag/serializers.py
+++ openbook_hashtags/views/hashtag/serializers.py
@@ -61,6 +61,7 @@ class GetHashtagPostsPostSerializer(serializers.ModelSerializer):
             'reaction',
             'is_edited',
             'is_closed',
+            'is_encirled',
             'media_height',
             'media_width',
             'media_thumbnail',
