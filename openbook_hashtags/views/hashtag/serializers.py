acf84331c47a2f8a156f53caa3f920e8fcfc91c5
Shantanu <shantanutarey@gmail.com>
2019-12-04 11:40:24 +0100

    :bug: add is_encircled to hashtag serializer

--- openbook_hashtags/views/hashtag/serializers.py
+++ openbook_hashtags/views/hashtag/serializers.py
@@ -61,6 +61,7 @@ class GetHashtagPostsPostSerializer(serializers.ModelSerializer):
             'reaction',
             'is_edited',
             'is_closed',
+            'is_encircled',
             'media_height',
             'media_width',
             'media_thumbnail',
